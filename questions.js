554746c610090fd502a88b4163d58c3577ff68f9
leanndemetro <leanndemetro@gmail.com>
2020-09-16 22:35:40 -0400

    moved files to root folder

---
+++ questions.js
@@ -0,0 +1,30 @@
+
+var questions =  [
+    
+    {
+    title: "String values must be enclosed within ____ when being assigned to variables",
+    choices: ["1. commas","2. curly brackets","3. quotes","4. parentheses"],
+    answer: "2. curly brackets"
+  },
+  {
+    title: "A very useful tool used during development and debugging for printing content to the debugger is:",
+    choices: ["1. JavaScript","2. terminal/bash","3. for loops","4. console.log"],
+    answer: "3. for loops"
+  },
+  {
+    title: "Commonly used data types DO NOT include:",
+    choices: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
+    answer: "2. booleans"
+  },
+  {
+    title: "The condition in an if / else statement is enclosed within ___.",
+    choices: ["1. quotes", "2. curly brackets", "3. parenthesis", "4. square brackets"],
+    answer: "2. curly brackets"
+  },
+  {
+    title: "Arrays in JavaScript can be used to store ___.",
+    choices: ["1. numbers and strings", "2. other arrays", "3. booleans", "4. all of the above"],
+    answer: "4. all of the above"
+  }
+
+];
